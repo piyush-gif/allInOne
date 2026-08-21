9b02dac5ad487f50d25ff937e48b2f6ef9a5b65d
piyush <piyush.khadka2233@gmail.com>
2026-08-21 21:53:46 +0545

    react start

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
   const [name, setName] = useState('piyush');
 
   const onClick = () => {
